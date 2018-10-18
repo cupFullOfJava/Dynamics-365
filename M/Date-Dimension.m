eef8415847965587e313594720409eb0bcf369fe
Jeremy Timothy Brown <jeremytimothybrown@gmail.com>
2018-10-18 09:44:28 -0400

    Set end date dynamically from now till 20 years in the future

--- M/Date-Dimension.m
+++ M/Date-Dimension.m
@@ -1,6 +1,6 @@
 let
     StartDate = #date(2000,1,1),
-    EndDate = #date(2125,12,31),
+    EndDate = Date.From(Date.AddYears(DateTime.LocalNow(), 20)),
     NumberOfDays = Duration.Days( EndDate - StartDate ),
     Dates = List.Dates(StartDate, NumberOfDays+1, #duration(1,0,0,0)),
     #"Converted to Table" = Table.FromList(Dates, Splitter.SplitByNothing(), null, null, ExtraValues.Error),
